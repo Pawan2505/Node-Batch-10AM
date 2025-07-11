9e5d90df839c7487108721d00c66217475fc33ac
pawan2505 <pawanaktu@gmail.com>
2025-07-11 20:40:19 +0530

    Nodemailer-forget_password

---
+++ 33_admin_panel_forget_password_otp/routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const admin = require('../models/admin');
+const adminCtl = require('../controllers/adminController');
+const {checkAdminAuth}  = require("../middleware/auth");
+
+const routes = express.Router();
+console.log("Routing...");
+
+// Public Routes
+routes.get('/', adminCtl.SignIn);
+routes.post('/checkLogin', adminCtl.checkLogin);
+routes.get('/changePassword',checkAdminAuth,adminCtl.changePassword);
+routes.post('/checkChangePassword', checkAdminAuth, adminCtl.checkChangePassword);
+routes.get('/profile',checkAdminAuth,adminCtl.profile)
+
+// forget password routing
+
+
+routes.get("/verifyEmail", adminCtl.verifyEmail)
+routes.post("/checkemailforget", adminCtl.checkemailforget)
+routes.get("/otp_page", adminCtl.otp_page)
+routes.post("/verifyOTP", adminCtl.verifyOTP)
+routes.get("/addNewPassword", adminCtl.addNewPassword)
+routes.post("/updatePassword", adminCtl.updatePassword) 
+
+// Protected Routes (admin only)
+routes.get('/dashboard', checkAdminAuth, adminCtl.dashboard);
+routes.get('/add_admin', checkAdminAuth, adminCtl.add_admin);
+routes.get('/view_admin', checkAdminAuth, adminCtl.view_admin);
+routes.post('/insertAdminData', checkAdminAuth, admin.upload, adminCtl.insertData);
+routes.get('/deleteAdmin/:id', checkAdminAuth, adminCtl.deleteData);
+routes.get('/editAdmin/:id', checkAdminAuth, adminCtl.editData);
+routes.post('/updateAdminData/:id', checkAdminAuth, admin.upload, adminCtl.updateData);
+
+// Logout
+routes.get('/logout', adminCtl.logout);
+
+module.exports = routes;
